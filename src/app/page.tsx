aa7c0d43e3ef4600f0f83d1e269777b62f7150ae
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-22 01:14:16 -0700

    Create hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Nav from "./components/nav/nav";
 import MobileMenu from "./components/mobile-menu/mobile-menu";
 import DesktopMenu from "./components/desktop-menu/desktop-menu";
 
+import styles from "./page.module.css";
+
 async function Home() {
   const session = await auth();
   const user = session?.user;
@@ -12,13 +14,35 @@ async function Home() {
   return (
     <>
       <header>
-        <Nav />
-        <DesktopMenu profileIcon={user?.image} />
-        <MobileMenu profileIcon={user?.image} />
+        <div className="header-content-wrapper">
+          <Nav />
+          <DesktopMenu profileIcon={user?.image} />
+          <MobileMenu profileIcon={user?.image} />
+        </div>
       </header>
       <main>
-        <p>coming soon</p>
-        <p>sign in, then see the mobile layout</p>
+        <section className={styles.hero}>
+          <div className={styles.backgroundLayer}>
+            <div className={styles.gradientOverlay}></div>
+            <div className={styles.radialOverlay}></div>
+            <div className={styles.gridOverlay}>
+              <div className={styles.animatedCircleOne}></div>
+              <div className={styles.animatedCircleTwo}></div>
+              <div className={styles.animatedCircleThree}></div>
+            </div>
+          </div>
+
+          <div className={styles.heroContentWrapper}>
+            <p className={styles.slogan}>Your opinion matters.</p>
+            <p className={styles.pollsterSummary}>
+              Create engaging music polls, vote on your favorite topics, publish
+              reviews, and discover like-minded people.
+            </p>
+            <button type="button" className={styles.getStarted}>
+              Get Started
+            </button>
+          </div>
+        </section>
       </main>
     </>
   );
