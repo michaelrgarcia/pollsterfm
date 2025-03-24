c371291a959a3db02e35bd9eb49f851299f78eea
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-23 22:53:58 -0700

    Add desktop menu

--- src/app/components/desktop-menu/desktop-menu.test.tsx
+++ src/app/components/desktop-menu/desktop-menu.test.tsx
@@ -1,8 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
+
+import userEvent from "@testing-library/user-event";
 
 import DesktopMenu from "./desktop-menu";
 
 // EXTEND FOR FLOATING UI TESTS
+
+const waitForPosition = () => act(async () => {});
 
 describe("desktop menu (logged in)", () => {
   const PROFILE_ICON =
@@ -12,10 +16,26 @@ describe("desktop menu (logged in)", () => {
     render(<DesktopMenu profileIcon={PROFILE_ICON} />);
 
     const signInBtn = screen.queryByRole("link", { name: "Sign In" });
-    const desktopMenu = screen.getByTitle("Menu");
+    const desktopMenu = screen.getByAltText("Menu");
 
     expect(signInBtn).not.toBeInTheDocument();
     expect(desktopMenu).toBeInTheDocument();
+  });
+
+  it("menu opens", async () => {
+    const user = userEvent.setup();
+
+    render(<DesktopMenu profileIcon={PROFILE_ICON} />);
+
+    const desktopMenu = screen.getByAltText("Menu");
+
+    await user.click(desktopMenu);
+
+    await waitForPosition();
+
+    const signOutBtn = screen.getByRole("menuitem", { name: "Sign Out" });
+
+    expect(signOutBtn).toBeInTheDocument();
   });
 });
 
@@ -23,7 +43,7 @@ describe("desktop menu (logged out)", () => {
   it("renders correctly", async () => {
     render(<DesktopMenu profileIcon={null} />);
 
-    const desktopMenu = screen.queryByTitle("Menu");
+    const desktopMenu = screen.queryByAltText("Menu");
     const signInBtn = screen.getByRole("link", { name: "Sign In" });
 
     expect(desktopMenu).not.toBeInTheDocument();
--- src/app/components/desktop-menu/desktop-menu.tsx
+++ src/app/components/desktop-menu/desktop-menu.tsx
@@ -5,6 +5,8 @@ import { signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { Menu } from "@base-ui-components/react/menu";
+
 import styles from "./desktop-menu.module.css";
 
 interface DesktopMenuProps {
@@ -19,14 +21,30 @@ function DesktopMenu({ profileIcon }: DesktopMenuProps) {
   return (
     <div className={styles.profileIconWrapper}>
       {profileIcon ? (
-        <button
-          type="button"
-          title="Menu"
-          className={styles.profileIcon}
-          onClick={handleSignOut}
-        >
-          <Image src={profileIcon} width={40} height={40} alt="Profile Icon" />
-        </button>
+        <Menu.Root>
+          <Menu.Trigger className={styles.profileIcon}>
+            <Image src={profileIcon} width={40} height={40} alt="Menu" />
+          </Menu.Trigger>
+          <Menu.Portal>
+            <Menu.Positioner
+              className={styles.Positioner}
+              sideOffset={19}
+              alignOffset={-50}
+            >
+              <Menu.Popup className={styles.Popup}>
+                <Menu.Item
+                  className={styles.Item}
+                  render={<Link href="profile">My Profile</Link>}
+                />
+                <Menu.Item className={styles.Item}>Account Settings</Menu.Item>
+                <Menu.Separator className={styles.Separator} />
+                <Menu.Item className={styles.Item} onClick={handleSignOut}>
+                  Sign Out
+                </Menu.Item>
+              </Menu.Popup>
+            </Menu.Positioner>
+          </Menu.Portal>
+        </Menu.Root>
       ) : (
         <Link href="/sign-in" className={styles.signInBtn}>
           Sign In
