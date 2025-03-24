c7a6a6092a5dbaca1e3c70aab473b611d7227d96
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-24 02:52:26 -0700

    Create custom 404 pg

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <main className="centered-main">
+      <h2 className="not-found-header">404 Not Found</h2>
+      <p className="not-found-error-text">Sorry! This page does not exist.</p>
+    </main>
+  );
+}
+
+export default NotFound;
