52c18f6af7028f80e8ad8bc7184229f3b3b94caa
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-24 01:31:35 -0700

    Put nav in root layout

--- src/app/components/nav/nav.tsx
+++ src/app/components/nav/nav.tsx
@@ -1,26 +1,42 @@
+import { auth } from "@/auth";
+
 import Link from "next/link";
+
+import DesktopMenu from "./desktop-menu/desktop-menu";
+import MobileMenu from "./mobile-menu/mobile-menu";
 
 import styles from "./nav.module.css";
 
-function Nav() {
+async function Nav() {
+  const session = await auth();
+  const user = session?.user;
+
   return (
-    <nav className={styles.navBar}>
-      <div className={styles.logoAndLinks}>
-        <h1 className={styles.logoText}>Pollster.fm</h1>
-        <ul className={styles.navLinks}>
-          <li>
-            <Link href="">Polls</Link>
-          </li>
-          <li>
-            <Link href="">Reviews</Link>
-          </li>
-          <li>
-            <Link href="">Community</Link>
-          </li>
-        </ul>
+    <header className={styles.pageHeader}>
+      <div className={styles.pageHeaderContentWrapper}>
+        <nav className={styles.navBar}>
+          <div className={styles.logoAndLinks}>
+            <h1 className={styles.logoText}>
+              <Link href="/">Pollster.fm</Link>
+            </h1>
+            <ul className={styles.navLinks}>
+              <li>
+                <Link href="">Polls</Link>
+              </li>
+              <li>
+                <Link href="">Reviews</Link>
+              </li>
+              <li>
+                <Link href="">Community</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.searchBar}>search...</div>
+        </nav>
+        <DesktopMenu profileIcon={user?.image} />
+        <MobileMenu profileIcon={user?.image} />
       </div>
-      <div className={styles.searchBar}>search...</div>
-    </nav>
+    </header>
   );
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from "./components/nav/nav";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable}`}>{children}</body>
+      <body className={`${inter.variable}`}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
