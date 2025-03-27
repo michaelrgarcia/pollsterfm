530902b9b04d74f87c271eab07b89d37cabe179e
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-27 01:38:51 -0700

    Establish profile pg struct.+display spotify deets

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,46 @@
 import { auth } from "@/auth";
 
+import Image from "next/image";
+
+import styles from "./page.module.css";
+
 async function Profile() {
   const session = await auth();
   const user = session?.user;
+
+  console.log(user);
+
+  return (
+    <main className={styles.profilePage}>
+      <div className={styles.profilePageWrapper}>
+        {user && (
+          <div className={styles.profileHeader}>
+            <div className={styles.userInfo}>
+              {user.image && (
+                <div className={styles.profileIcon}>
+                  <Image
+                    src={user.image}
+                    alt="Profile Icon"
+                    fill
+                    sizes="100%"
+                  />
+                </div>
+              )}
+              <div className={styles.displayAndUser}>
+                <div className={styles.displayAndPronouns}>
+                  {user.name && (
+                    <p className={styles.displayName}>{user.name}</p>
+                  )}
+                  <p className={styles.pronouns}>none (PLACEHOLDER)</p>
+                </div>
+                <p className={styles.username}>@PLACEHOLDER</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </main>
+  );
 
   return <p>{user?.name}</p>;
 }
