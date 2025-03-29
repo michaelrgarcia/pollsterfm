377d04a64a3cb839651f5e713bff855cad3d5e6b
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-28 17:17:38 -0700

    Add prisma adapter to store users in db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import Spotify from "next-auth/providers/spotify";
 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { prisma } from "./prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     Spotify({
       authorization:
