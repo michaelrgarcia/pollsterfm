9300f7fd6733d298c8dd19229de4d4768fef6508
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-30 23:51:53 -0700

    Remove unnecessary info from AdapterUser type

--- src/lib/types/adapterTypes.d.ts
+++ src/lib/types/adapterTypes.d.ts
@@ -2,9 +2,6 @@ export type Awaitable<T> = T | PromiseLike<T>;
 
 export type AdapterUser = {
   email: string;
-  emailVerified: null | Date;
-  id: string;
   image: null | string;
-  name: string;
   username: string;
 };
