151c88969abc453bdc1d76dc37cf0e4179ff6e1f
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-30 23:50:03 -0700

    Remove pollsterUser cast

--- src/app/my-profile/page.tsx
+++ src/app/my-profile/page.tsx
@@ -1,12 +1,10 @@
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
-import { type PollsterUser } from "@/lib/types/pollsterUser";
-
 async function MyProfile() {
   const session = await auth();
 
-  const user = session?.user as PollsterUser;
+  const user = session?.user;
 
   if (user) {
     return redirect(`/user/${user.username}`);
