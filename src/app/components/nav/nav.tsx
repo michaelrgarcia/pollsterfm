bfe395710e45dd67c30b6e134e545ad13f73324d
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-30 23:56:32 -0700

    Dummy commit

--- src/app/components/nav/nav.tsx
+++ src/app/components/nav/nav.tsx
@@ -5,13 +5,11 @@ import Link from "next/link";
 import DesktopMenu from "./desktop-menu/desktop-menu";
 import MobileMenu from "./mobile-menu/mobile-menu";
 
-import { type PollsterUser } from "@/lib/types/pollsterUser";
-
 import styles from "./nav.module.css";
 
 async function Nav() {
   const session = await auth();
-  const user = session?.user as PollsterUser;
+  const user = session?.user;
 
   return (
     <header className={styles.pageHeader}>
