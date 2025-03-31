55536cdbe68e48c284dcba705f6bcd6c44b043c2
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-30 23:52:11 -0700

    Remove caster

--- src/lib/types/pollsterUser.d.ts
+++ src/lib/types/pollsterUser.d.ts
@@ -1,16 +1,3 @@
-export type PollsterUser = {
-  id: string;
-  name: string;
-  username: string;
-  pronouns: null | string;
-  aboutMe: null | string;
-  email: string;
-  emailVerified: null | Date;
-  image: null | string;
-  createdAt: string;
-  updatedAt: string;
-};
-
 export type PollsterProfile = {
   pronouns: string;
   aboutMe: string;
