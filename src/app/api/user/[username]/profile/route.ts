d42759ae5634dc78e614be00e04ce7a7cba9628f
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-04-13 23:24:12 -0700

    Update profile api route imports

--- src/app/api/user/[username]/profile/route.ts
+++ src/app/api/user/[username]/profile/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { getProfile } from "@/lib/actions";
+import { getProfile } from "@/lib/data-access/user";
 
 type RequestParams = {
   params: Promise<{ username: string }>;
