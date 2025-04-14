0a3a42316d881f9cd8bd72fc9c5d6bd8977d575f
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-04-14 03:05:17 -0700

    Create schemas / validators for edit profile

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,20 +1,39 @@
 import { z } from "zod";
 
 const imageFileSizeLimit = 5 * 1024 * 1024; // 5MB
-const imageFile = z
+
+export const headerImageSchema = z
+  .instanceof(File)
+  .refine(
+    (file) => ["image/png", "image/jpeg", "image/webp"].includes(file.type),
+    {
+      message:
+        "Invalid file type. Accepted file types: .png, .jpeg, and .webp.",
+    }
+  )
+  .refine((file) => file.size <= imageFileSizeLimit, {
+    message: "File size should not exceed 5MB.",
+  });
+
+export const profileIconSchema = z
   .instanceof(File)
   .refine(
     (file) =>
-      ["image/png", "image/jpeg", "image/jpg", "image/gif"].includes(file.type),
-    { message: "Accepted file types are .png, .jpeg, .jpg, .webp, and .gif." }
+      ["image/png", "image/jpeg", "image/webp", "image/gif"].includes(
+        file.type
+      ),
+    {
+      message:
+        "Invalid file type.  Accepted file types: .png, .jpeg, .webp, and .gif.",
+    }
   )
   .refine((file) => file.size <= imageFileSizeLimit, {
     message: "File size should not exceed 5MB.",
   });
 
 export const profileSchema = z.object({
-  headerImage: imageFile,
-  profileIcon: imageFile,
+  headerImage: headerImageSchema,
+  profileIcon: profileIconSchema,
   name: z.string().min(1).max(15),
   username: z.string().min(1).max(20),
   aboutMe: z.string().max(250),
