b539c54d5530779823afcc283cc8d719c5ddf07b
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-04-18 03:05:20 -0700

    Make properties nullable

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -2,39 +2,45 @@ import { z } from "zod";
 
 const imageFileSizeLimit = 5 * 1024 * 1024; // 5MB
 
-export const headerImageSchema = z
-  .instanceof(File)
-  .refine(
-    (file) => ["image/png", "image/jpeg", "image/webp"].includes(file.type),
-    {
-      message:
-        "Invalid file type. Accepted file types: .png, .jpeg, and .webp.",
-    }
-  )
-  .refine((file) => file.size <= imageFileSizeLimit, {
-    message: "File size should not exceed 5MB.",
-  });
+export const headerImageSchema = z.nullable(
+  z
+    .instanceof(File)
+    .refine(
+      (file) => ["image/png", "image/jpeg", "image/webp"].includes(file.type),
+      {
+        message:
+          "Invalid file type. Accepted file types: .png, .jpeg, and .webp.",
+      }
+    )
+    .refine((file) => file.size <= imageFileSizeLimit, {
+      message: "File size should not exceed 5MB.",
+    })
+);
 
-export const profileIconSchema = z
-  .instanceof(File)
-  .refine(
-    (file) =>
-      ["image/png", "image/jpeg", "image/webp", "image/gif"].includes(
-        file.type
-      ),
-    {
-      message:
-        "Invalid file type. Accepted file types: .png, .jpeg, .webp, and .gif.",
-    }
-  )
-  .refine((file) => file.size <= imageFileSizeLimit, {
-    message: "File size should not exceed 5MB.",
-  });
+export const profileIconSchema = z.nullable(
+  z
+    .instanceof(File)
+    .refine(
+      (file) =>
+        ["image/png", "image/jpeg", "image/webp", "image/gif"].includes(
+          file.type
+        ),
+      {
+        message:
+          "Invalid file type. Accepted file types: .png, .jpeg, .webp, and .gif.",
+      }
+    )
+    .refine((file) => file.size <= imageFileSizeLimit, {
+      message: "File size should not exceed 5MB.",
+    })
+);
 
-export const profileSchema = z.object({
-  headerImage: headerImageSchema,
-  profileIcon: profileIconSchema,
-  name: z.string().min(1).max(15),
-  username: z.string().min(1).max(20),
-  aboutMe: z.string().max(250),
+export const editProfileSchema = z.object({
+  newHeaderImg: headerImageSchema,
+  newProfileIcon: profileIconSchema,
+  newName: z.string().min(1).max(15),
+  newUsername: z.string().min(1).max(25),
+  newAboutMe: z.nullable(z.string().max(250)),
+  oldHeaderImg: z.nullable(z.string()),
+  oldProfileIcon: z.nullable(z.string()),
 });
