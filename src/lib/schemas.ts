3358f14d22725db19b8851f745b406c9682cc018
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-04-19 01:08:17 -0700

    Add more validators to profile form fields

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const imageFileSizeLimit = 5 * 1024 * 1024; // 5MB
+const imageFileSizeLimit = 5 * 1024 * 1024;
 
 export const headerImageSchema = z.nullable(
   z
@@ -38,9 +38,31 @@ export const profileIconSchema = z.nullable(
 export const editProfileSchema = z.object({
   newHeaderImg: headerImageSchema,
   newProfileIcon: profileIconSchema,
-  newName: z.string().min(1).max(15),
-  newUsername: z.string().min(1).max(25),
-  newAboutMe: z.nullable(z.string().max(250)),
+  newName: z
+    .string()
+    .trim()
+    .min(1, { message: "Name must be at least 1 character long." })
+    .max(15, { message: "Name cannot be longer than 15 characters." }),
+  newUsername: z
+    .string()
+    .trim()
+    .refine((value) => !/\s/.test(value), {
+      message: "Username cannot contain spaces.",
+    })
+    .transform((value) => value.replace(/\s+/g, ""))
+    .pipe(
+      z
+        .string()
+        .toLowerCase()
+        .min(3, { message: "Username must be at least 3 characters long." })
+        .max(25, { message: "Username cannot be longer than 25 characters." })
+    ),
+  newAboutMe: z.nullable(
+    z
+      .string()
+      .max(250, { message: "About Me cannot be longer than 250 characters." })
+      .trim()
+  ),
   oldHeaderImg: z.nullable(z.string()),
   oldProfileIcon: z.nullable(z.string()),
 });
