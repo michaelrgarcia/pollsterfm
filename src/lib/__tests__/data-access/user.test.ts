b74dfe4cb25e56db96775fcb87486a86c9ae40d4
Michael Garcia dev <{ID}+{username}@users.noreply.github.com>
2025-04-20 18:50:25 -0700

    Temp ocmmit for other machine

--- src/lib/__tests__/data-access/user.test.ts
+++ src/lib/__tests__/data-access/user.test.ts
@@ -19,42 +19,62 @@ vi.mock("../../../lib/auth", async () => ({
   }),
 }));
 
-vi.mock("../../../lib/supabase", () => ({
-  supabase: supabaseMock,
-}));
+describe("user data access", () => {
+  describe("updateProfile", () => {
+    beforeEach(() => {
+      vi.doMock("../../../lib/supabase", () => ({
+        supabase: supabaseMock,
+      }));
 
-describe("updateProfile", () => {
-  // must be mocked before each test
-  // (updateProfile is defined after other methods that use prisma)
-  beforeEach(() => {
-    vi.doMock("../../../lib/prisma", async () => ({
-      prisma: prismaMock,
-    }));
-  });
+      vi.doMock("../../../lib/prisma", async () => ({
+        prisma: prismaMock,
+      }));
+    });
 
-  it("redirects unauthenticated user", async () => {
-    vi.doMock("../../../lib/auth", () => ({
-      auth: vi.fn().mockResolvedValue(null),
-    }));
+    it("redirects unauthenticated user", async () => {
+      vi.doMock("../../../lib/auth", () => ({
+        auth: vi.fn().mockResolvedValue(null),
+      }));
 
-    // dynamic import required
-    // (prisma needs to be mocked before this is imported)
-    const { updateProfile } = await import("../../../lib/data-access/user");
+      // dynamic import required
+      // (prisma needs to be mocked before this is imported)
+      const { updateProfile } = await import("../../../lib/data-access/user");
 
-    const mockFormData = {
-      newHeaderImg: null,
-      newProfileIcon: null,
-      newName: "",
-      newUsername: "",
-      newAboutMe: "",
-      oldHeaderImg: "",
-      oldProfileIcon: "",
-      deleteHeaderImg: false,
-      deleteProfileIcon: false,
-    };
+      const mockFormData = {
+        newHeaderImg: null,
+        newProfileIcon: null,
+        newName: "",
+        newUsername: "",
+        newAboutMe: "",
+        oldHeaderImg: "",
+        oldProfileIcon: "",
+        deleteHeaderImg: false,
+        deleteProfileIcon: false,
+      };
 
-    await updateProfile(mockFormData);
+      await updateProfile(mockFormData);
 
-    expect(redirect).toHaveBeenCalledWith("/sign-in");
+      expect(redirect).toHaveBeenCalledWith("/sign-in");
+    });
+
+    // it("deletes existing header image", async () => {
+    //     const { updateProfile } = await import("../../../lib/data-access/user");
+
+    //     const mockFormData = {
+    //       newHeaderImg: null,
+    //       newProfileIcon: null,
+    //       newName: "",
+    //       newUsername: "",
+    //       newAboutMe: "",
+    //       oldHeaderImg: "https://cdn.supabase.mock/storage/v1/object/public/header-images//reviews-web-1-0-elliott-smith-show-website-v0-pvb2jucu3oob1.webp",
+    //       oldProfileIcon: "",
+    //       deleteHeaderImg: true,
+    //       deleteProfileIcon: false,
+    //     };
+
+    //     await updateProfile(mockFormData);
+
+    //     expect(redirect).toHaveBeenCalledWith("/sign-in");
+    //   });
   });
 });
