fd4d0f727956b444932d6cb64e24cafbb3d7ae18
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-04-30 03:16:09 -0700

    Type artist-related data fetching

--- src/lib/types/serverResponses.d.ts
+++ src/lib/types/serverResponses.d.ts
@@ -4,3 +4,11 @@ export type UpdateProfileResult =
       success: false;
       errors: { path: (string | number)[]; message: string }[];
     };
+
+export type FirstArtistResult = {
+  name: string;
+  image: string | null;
+  genres: string[] | Tag[] | null;
+  spotifyUrl: string | null;
+  lastfmUrl: string | null;
+};
--- src/lib/types/spotify.d.ts
+++ src/lib/types/spotify.d.ts
@@ -8,6 +8,22 @@ export interface Image {
   url: string;
 }
 
+export type Artist = {
+  genres: string[];
+  /**
+   * The {@link https://developer.spotify.com/documentation/web-api/concepts/spotify-uris-ids|Spotify ID} for the artist.
+   */
+  id: string;
+  images: Image[];
+  name: string;
+  external_urls: {
+    /**
+     * The {@link https://developer.spotify.com/documentation/web-api/concepts/spotify-uris-ids|Spotify URL} for the object.
+     */
+    spotify: string;
+  };
+};
+
 export interface Album {
   name: string;
   artists: SimplifiedArtist[];
