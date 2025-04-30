261445e84bedcf30c0e8bfd3e2ecc37452000fda
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-04-30 03:14:46 -0700

    Organize responses (mainly for last fm)

---
+++ src/lib/types/spotifyResponses.d.ts
@@ -0,0 +1,32 @@
+import type { Artist, PlayHistory, Track } from "./spotify";
+
+export type SpotifyAccessTokenResponse = {
+  access_token: string;
+  token_type: "Bearer";
+  expires_in: 3600;
+  refresh_token: string;
+  scope: string;
+};
+
+export type SpotifyClientCredentialsResponse = {
+  access_token: stirng;
+  token_type: "Bearer";
+  expires_in: 3600;
+};
+
+export type SpotifyCurrentlyPlayingResponse = {
+  progress_ms: number;
+  item: Track;
+};
+
+export type SpotifyRecentlyPlayedResponse = {
+  next: string | null;
+  progress_ms: number;
+  items: PlayHistory[];
+};
+
+export type SpotifyArtistSearchResponse = {
+  artists: {
+    items: Artist[];
+  };
+};
