379e49d4330819e37dc0f7fa3ef53a5255f1ed05
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-04-30 03:13:07 -0700

    Get artist from spotify+lastfm+ make responsive

--- src/app/catalog/[artist]/page.tsx
+++ src/app/catalog/[artist]/page.tsx
@@ -1,38 +1,41 @@
-import { type Metadata } from "next";
-
 import ArtistAffinities from "@/app/components/artist-affinities/artist-affinities";
 import ArtistHeader from "@/app/components/artist-header/artist-header";
 import Discography from "@/app/components/discography/discography";
 import FeaturedIn from "@/app/components/featured-in/featured-in";
 import SimilarArtists from "@/app/components/similar-artists/similar-artists";
 import TopListeners from "@/app/components/top-listeners/top-listeners";
+import { getCachedArtist } from "@/lib/pollster/artist";
+import { redirect } from "next/navigation";
 import styles from "./page.module.css";
 
 type ArtistProps = {
   params: Promise<{ artist: string }>;
 };
 
-export const metadata: Metadata = {
-  title: "(artist) | Pollster.fm",
-  description: "Find more about (artist) on Pollster.fm.",
-};
+export async function generateMetadata({ params }: ArtistProps) {
+  const { artist } = await params;
+
+  const artistData = await getCachedArtist(artist);
+
+  if (!artistData) redirect("/not-found");
+
+  return {
+    title: `${artistData.name} | Pollster.fm`,
+    description: `Find more about ${artistData.name} on Pollster.fm.`,
+  };
+}
 
 async function Artist({ params }: ArtistProps) {
   const { artist } = await params;
 
-  // if (!artist) return redirect("/not-found");
-
-  // look for img on spotify first
-
-  // make ALL of these components more responsive
-
   return (
     <main className={styles.pageContainer}>
-      <ArtistHeader />
+      <ArtistHeader artistName={artist} />
       <div className={styles.contentContainer}>
         <div className={styles.contentGrid}>
           <aside className={styles.sidebar}>
             <ArtistAffinities />
+
             <SimilarArtists />
           </aside>
           <div className={styles.mainContent}>
