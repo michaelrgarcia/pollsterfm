6865f4a6ad8237e618b96170afc0d9e5bd2a2ba9
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-05-04 18:46:59 -0700

    Return null if no artist data

--- src/app/components/artist-header/artist-header.tsx
+++ src/app/components/artist-header/artist-header.tsx
@@ -9,7 +9,7 @@ type ArtistHeaderProps = {
 async function ArtistHeader({ artistName }: ArtistHeaderProps) {
   const artistData = await getCachedArtist(artistName);
 
-  if (!artistData) return <p>No artist found.</p>;
+  if (!artistData) return null;
 
   return (
     <ClientArtistHeader artistData={artistData} originalQuery={artistName} />
