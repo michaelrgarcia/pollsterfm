b78df7c4c12685165c6666df6129411881b80336
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-05-11 22:17:19 -0700

    Make title bold and desc light

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
 function NotFound() {
   return (
-    <main className="flex min-h-[80svh] w-full flex-col items-center justify-center">
-      <h2 className="mb-5 text-3xl font-semibold">404 Not Found</h2>
-      <p className="text-xl">Sorry! This page does not exist.</p>
+    <main className="centered-main">
+      <h2 className="mb-5 text-3xl font-bold">404 Not Found</h2>
+      <p className="text-xl font-light">Sorry! This page does not exist.</p>
     </main>
   );
 }
