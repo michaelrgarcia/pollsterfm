c62c669b43b1101d445a90de05e4481ae9775bc5
Michael Garcia dev <{ID}+{username}@users.noreply.github.com>
2025-05-11 17:36:11 -0700

    Make not found pg + generate prisma

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <main className="flex h-[80%] w-full flex-col items-center justify-center">
+      <h2 className="mb-5 text-3xl font-semibold">404 Not Found</h2>
+      <p className="text-xl">Sorry! This page does not exist.</p>
+    </main>
+  );
+}
+
+export default NotFound;
