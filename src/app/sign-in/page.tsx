5a532c6feb0cf6770ad64dd0e376766c02d5eac5
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-05-12 00:08:11 -0700

    No chg to sign in page

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,27 @@
+import { auth } from "@/lib/auth";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+
+import { siteName } from "@/config";
+import ProviderLogins from "../components/provider-logins/provider-logins";
+
+export const metadata: Metadata = {
+  title: `Sign In | ${siteName}`,
+  description: `Sign in to ${siteName}`,
+};
+
+async function SignIn() {
+  const session = await auth();
+
+  const user = session?.user;
+
+  if (user) return redirect("/profile");
+
+  return (
+    <main className="centered-main">
+      <ProviderLogins />
+    </main>
+  );
+}
+
+export default SignIn;
