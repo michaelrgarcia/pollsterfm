a1696022bf812885feb85add86a44d26a0eed547
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-05-20 16:55:10 -0700

    Attempt to create dropdown menu component

---
+++ src/app/components/ui/dropdown-menu.tsx
@@ -0,0 +1,76 @@
+import { Menu as DropdownMenuPrimitive } from "@base-ui-components/react/menu";
+import {
+  forwardRef,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+} from "react";
+
+import { cn } from "@/lib/utils";
+
+const DropdownMenu = DropdownMenuPrimitive.Root;
+
+const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
+
+// const DropdownMenuGroup = DropdownMenuPrimitive.Group;
+
+// const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
+
+const DropdownMenuContent = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Popup>,
+  ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Positioner>
+>(({ className, sideOffset = 0, alignOffset = 0, ...props }, ref) => (
+  <DropdownMenuPrimitive.Portal>
+    <DropdownMenuPrimitive.Positioner
+      alignOffset={alignOffset}
+      sideOffset={sideOffset}
+      className="isolate z-25 outline-0"
+      {...props}
+    >
+      <DropdownMenuPrimitive.Popup
+        ref={ref}
+        className={cn(
+          "bg-popover text-popover-foreground outline-border origin-[var(--transform-origin)] rounded-md py-1 shadow-lg shadow-gray-200 outline transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1",
+          className,
+        )}
+      />
+    </DropdownMenuPrimitive.Positioner>
+  </DropdownMenuPrimitive.Portal>
+));
+DropdownMenuContent.displayName = DropdownMenuPrimitive.Popup.displayName;
+
+const DropdownMenuItem = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & {
+    inset?: boolean;
+  }
+>(({ className, ...props }, ref) => (
+  <DropdownMenuPrimitive.Item
+    ref={ref}
+    className={cn(
+      "data-[highlighted]:before:bg-popover/8 flex cursor-pointer px-2.5 py-1.5 text-sm leading-4 outline-0 select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm",
+      className,
+    )}
+    {...props}
+  />
+));
+DropdownMenuItem.displayName = DropdownMenuPrimitive.Item.displayName;
+
+const DropdownMenuSeparator = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Separator>,
+  ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Separator>
+>(({ className, ...props }, ref) => (
+  <DropdownMenuPrimitive.Separator
+    ref={ref}
+    className={cn("bg-muted mx-4 my-1.5 h-px", className)}
+    {...props}
+  />
+));
+DropdownMenuSeparator.displayName = DropdownMenuPrimitive.Separator.displayName;
+
+export {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+};
