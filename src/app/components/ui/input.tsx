b1e3e301fbea8bdb02b4e7ac4648c32741ceedda
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-05-20 18:55:04 -0700

    Change hover + focus color to prevent confusion

--- src/app/components/ui/input.tsx
+++ src/app/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = forwardRef<HTMLInputElement, ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "border-input placeholder:text-muted-foreground focus-visible:ring-ring hover:border-ring/50 flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "border-input placeholder:text-muted-foreground focus-visible:ring-muted-foreground hover:border-muted-foreground/50 flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
--- src/app/components/ui/textarea.tsx
+++ src/app/components/ui/textarea.tsx
@@ -7,7 +7,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, ComponentProps<"textarea">>(
     return (
       <textarea
         className={cn(
-          "border-input placeholder:text-muted-foreground/50 focus-visible:ring-ring hover:border-ring/50 flex min-h-[100px] w-full rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "border-input placeholder:text-muted-foreground/50 focus-visible:ring-muted-foreground hover:border-muted-foreground/50 flex min-h-[100px] w-full rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
