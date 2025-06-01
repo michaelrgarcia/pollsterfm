c2764459cff7683353c1da6d6a642269725fc02c
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-05-31 23:47:22 -0700

    CReate temp function for spotify history tests

--- src/lib/data-access/user/read.ts
+++ src/lib/data-access/user/read.ts
@@ -55,3 +55,16 @@ export async function getName(username: string) {
     return null;
   }
 }
+
+/**
+ * Checks if a pollster.fm user has imported their Spotify listening history imported.
+ *
+ * @param username A pollster.fm user's username,
+ * @returns A boolean.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function spotifyHistoryImported(username: string) {
+  // TEMPORARY
+
+  return false;
+}
