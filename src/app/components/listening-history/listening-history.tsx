6af8c78165af81f4c7c2f636f86d9a82df0ec9e5
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-05-31 23:47:54 -0700

    Lay foundation for future platforms

--- src/app/components/listening-history/listening-history.tsx
+++ src/app/components/listening-history/listening-history.tsx
@@ -1,12 +1,17 @@
 import { Platform } from "@/config";
+import { spotifyHistoryImported } from "@/lib/data-access/user/read";
 import SpotifyListeningHistory from "./spotify/spotify";
 
-function ListeningHistory() {
-  // TEMP
+type ListeningHistoryProps = {
+  username: string;
+};
+
+async function ListeningHistory({ username }: ListeningHistoryProps) {
   const platform: Platform = "spotify";
+  const hasImported = await spotifyHistoryImported(username);
 
   return platform === "spotify" ? (
-    <SpotifyListeningHistory />
+    <SpotifyListeningHistory historyImported={hasImported} />
   ) : (
     <p>not implemented yet</p>
   );
