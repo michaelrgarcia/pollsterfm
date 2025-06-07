dbbb4462db3c010ab2ddb9fe2148be1dca4e8a94
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-06-07 01:36:54 -0700

    delete binary copy script

--- scripts/copy-prisma-binary.sh
+++
@@ -1,6 +0,0 @@
-PRISMA_BINARY="node_modules/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node"
-TARGET_DIR="src/lib/prisma/client" 
-
-mkdir -p "$TARGET_DIR"
-
-cp "$PRISMA_BINARY" "$TARGET_DIR/"
