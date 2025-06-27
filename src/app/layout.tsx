897460783f319f96b97895eddd73289a69fa7e42
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-06-26 22:06:37 -0700

    convex setup step

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Toast } from "@base-ui-components/react/toast";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
 import { siteName } from "../config";
@@ -6,9 +7,8 @@ import { toastManager } from "../lib/toast";
 import Nav from "./components/nav/nav";
 import { ThemeProvider } from "./components/theme-provider";
 
-import { Suspense } from "react";
+import { ConvexClientProvider } from "./components/ConvexClientProvider";
 import Footer from "./components/footer/footer";
-import NavSkeleton from "./components/nav/skeleton";
 import ToastList from "./components/ui/toast-list";
 import "./globals.css";
 
@@ -29,30 +29,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${lexend.variable} antialiased`}
-      suppressHydrationWarning
-    >
-      <body className="min-h-svh">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toast.Provider toastManager={toastManager}>
-            <Suspense fallback={<NavSkeleton />}>
-              <Nav />
-            </Suspense>
-            {children}
-            <Footer />
-            <Toast.Viewport className="fixed top-auto right-4 bottom-4 left-auto mx-auto my-0 w-62.5 sm:right-8 sm:bottom-8 sm:w-75">
-              <ToastList />
-            </Toast.Viewport>
-          </Toast.Provider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ConvexAuthNextjsServerProvider>
+      <html
+        lang="en"
+        className={`${lexend.variable} antialiased`}
+        suppressHydrationWarning
+      >
+        <body className="min-h-svh">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toast.Provider toastManager={toastManager}>
+              <ConvexClientProvider>
+                <Nav />
+              </ConvexClientProvider>
+              {children}
+              <Footer />
+              <Toast.Viewport className="fixed top-auto right-4 bottom-4 left-auto mx-auto my-0 w-62.5 sm:right-8 sm:bottom-8 sm:w-75">
+                <ToastList />
+              </Toast.Viewport>
+            </Toast.Provider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ConvexAuthNextjsServerProvider>
   );
 }
