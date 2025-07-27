8fc31eaaca26bd9b98efa6daa73f8c15e04d6784
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-07-27 01:39:17 -0700

    add top margin to h2

--- src/app/polls/page.tsx
+++ src/app/polls/page.tsx
@@ -3,7 +3,7 @@ import Polls from "../components/polls/polls";
 function PollsPage() {
   return (
     <main className="content-wrapper px-5 py-8 xl:p-0">
-      <h2 className="mb-6 text-3xl font-bold">Polls</h2>
+      <h2 className="my-6 text-3xl font-bold">Polls</h2>
       <Polls />
     </main>
   );
