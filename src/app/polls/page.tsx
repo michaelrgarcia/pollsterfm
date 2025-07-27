bd61c5acb9fe5ea2f85028a6d871cabf9440912d
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-07-27 01:43:23 -0700

    adjust padding

--- src/app/polls/page.tsx
+++ src/app/polls/page.tsx
@@ -2,7 +2,7 @@ import Polls from "../components/polls/polls";
 
 function PollsPage() {
   return (
-    <main className="content-wrapper px-5 py-8 xl:p-0">
+    <main className="content-wrapper px-5 pt-4 pb-8 xl:px-0">
       <h2 className="my-6 text-3xl font-bold">Polls</h2>
       <Polls />
     </main>
